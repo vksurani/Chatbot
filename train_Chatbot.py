6d365f9ebc6f0f26e6f83871d01611547238facc
vasusurani <vasusurani22@gmail.com>
2020-11-03 20:28:46 -0500

    Initializing Chatbot

---
+++ train_Chatbot.py
@@ -0,0 +1,12 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import random
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import json
+import pickle
+intents_file = open('intents.json').read()
+intents = json.loads(intents_file)
