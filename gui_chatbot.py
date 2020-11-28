581c608eaa739bc7de940ff2f93ff6237990e78b
vasusurani <vasusurani22@gmail.com>
2020-11-27 23:52:23 -0500

    Finished

--- gui_chatbot.py
+++ gui_chatbot.py
@@ -10,8 +10,8 @@ model = load_model('chatbot_model.h5')
 import json
 import random
 intents = json.loads(open('intents.json').read())
-words = pickle.load(open('words.pk1','rb'))
-classes = pickle.load(open('classes.pk1','rb'))
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
 def clean_up_sentance(sentance):
     # splitting words into array
     sentance_words = nltk.word_tokenize(sentance)
@@ -52,4 +52,41 @@ def getResponse(ints, intents_json):
             result = random.choice(i['responses'])
             break
     return result
+# creating GUI with tkinter
+import tkinter
+from tkinter import *
+def send():
+    msg = EntryBox.get("1.0","end-1c").strip()
+    EntryBox.delete("0.0",END)
+    if msg != '':
+        ChatBox.config(state=NORMAL)
+        ChatBox.insert(END, "You: "+ msg +'\n\n')
+        ChatBox.config(foreground="#446665", font=("Verdana", 12))
+        ints = predict_class(msg)
+        res = getResponse(ints, intents)
+
+        ChatBox.insert(END, "Bot: " + res + '\n\n')
+        ChatBox.config(state=DISABLED)
+        ChatBox.yview(END)
+root = Tk()
+root.title("Chatbot")
+root.geometry("400x500")
+root.resizable(width=FALSE, height=FALSE)
+# Creating chat window
+ChatBox = Text(root, bd=0, bg="white", height="8", width="50", font="Arial")
+ChatBox.config(state=DISABLED)
+# Bind scrollbar to chat window
+scrollbar = Scrollbar(root, command=ChatBox.yview, cursor="heart")
+ChatBox['yscrollcommand'] = scrollbar.set
+# Create button to send message
+SendButton = Button(root, font=("Verdana",12,'bold'), text="Send", width="12", height=5, bd=0, bg="#f9a602", activebackground="#3c9d9b", fg="#000000", command= send)
+# Create the box to enter message
+EntryBox = Text(root, bd=0, bg="white", width="29", height="5", font="Arial")
+# Entrybox.bind("<Return>", send)
+# Placing all components to the screen
+scrollbar.place(x=376,y=6,height=386)
+ChatBox.place(x=6,y=6,height=386,width=370)
+EntryBox.place(x=128,y=401,height=90,width=265)
+SendButton.place(x=6,y=401,height=90)
+root.mainloop()
 
